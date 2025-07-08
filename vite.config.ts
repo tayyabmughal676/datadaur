63bcc74ae76100055004cf3a8b9ae5331960d5ab
HussainDCode <hussain@datumcode.com>
2025-07-08 23:35:20 +0500

    remove vite-plugin-sitemap plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import sitemap from 'vite-plugin-sitemap';
+// import sitemap from 'vite-plugin-sitemap'; // 1. REMOVE THIS LINE
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-      react(),
-      tailwindcss(),
-      sitemap({ hostname: 'https://datadaur.vercel.app' })
+    react(),
+    tailwindcss(),
+    // sitemap({ hostname: 'https://datadaur.vercel.app' }) // 2. AND REMOVE THIS LINE
   ],
   resolve: {
     alias: {
